fc66899a86d1648366fd85aa6e1eecba5f8008e2
Artem Vashchuk <artem.vashchuk@gmail.com>
2025-03-11 12:25:59 +0200

    DEV-0002: Add event store db

---
+++ ExploringKafkaEventProcessing/BankAccount.cs
@@ -0,0 +1,9 @@
+﻿namespace ExploringKafkaEventProcessing;
+
+public class BankAccount
+{
+    public decimal Balance { get; private set; }
+
+    public void Apply(MoneyDeposited moneyDeposited) =>
+        Balance += moneyDeposited.Amount;
+}
---
+++ ExploringKafkaEventProcessing/EventStoreSubscriptionService.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using System.Text.Json;
+using EventStore.Client;
+
+namespace ExploringKafkaEventProcessing;
+
+public class EventStoreSubscriptionService(EventStoreClient client) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+    {
+        await client.SubscribeToStreamAsync(
+            "bank-account-1",
+            FromStream.Start,
+            async (_, @event, _) => await HandleMoneyDepositedEvent(@event),
+            cancellationToken: cancellationToken
+        );
+    }
+
+    private Task HandleMoneyDepositedEvent(ResolvedEvent resolvedEvent)
+    {
+        var json = Encoding.UTF8.GetString(resolvedEvent.Event.Data.Span);
+        var @event = JsonSerializer.Deserialize<MoneyDeposited>(json);
+        Console.WriteLine($"New deposit detected: {@event?.Amount}");
+
+        return Task.CompletedTask;
+    }
+}
